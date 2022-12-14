8cc7ec93367e0a499b8692cf0bfd9ab444b410e4
DevinAugot <105358840+DevinAugot@users.noreply.github.com>
2022-12-14 14:32:29 -0330

    Create SignUp.jsx

---
+++ SignUp.jsx
@@ -0,0 +1,87 @@
+import Header from './Header'
+import React, { useState } from "react";
+export const Register = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
+  const submit = (e) => {
+    console.log(
+      `Email: ${email}, Name: ${name}, username: ${user}, Password: ${pass}`
+    );
+    e.preventDefault();
+  };
+  return (
+    <div id="main">
+       <Header></Header>
+      <div>
+        <form onSubmit={submit} id="form-control">
+       
+          <p id="intro">
+            Sign up to see code/projects/photos from friends & other people
+            around the world! Even get help debugging your code!
+          </p>
+          <div className="Input_container">
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="Email"
+            id="email"
+          />{" "}
+          <br />
+          <br />
+          
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Full Name"
+            id="name"
+          />{" "}
+          <br />
+          <br />
+          
+          <input
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+            type="text"
+            placeholder="User Name"
+            id="user"
+          />{" "}
+          <br />
+          <br />
+          <input
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
+            type="password"
+            placeholder="Password"
+            id="pass"
+          />{" "}
+          <br />
+          <br />
+          </div>
+          <button type="submit" id="sign">
+            Sign Up
+          </button>
+          
+          <br />
+          <p id="terms">
+            By signing this you agree to our{" "}
+            <strong>Terms Privacy Policy</strong> and{" "}
+            <strong>Cookie Policy</strong>
+          </p>
+        </form>
+      </div>
+      <br />
+    
+    
+     <div id="login">
+     <p>
+       Have an account? <a href="http://localhost:3000/LoginPage">Log in</a>
+     </p>
+   </div>
+   </div>
+  );
+};
+ export default Register;
