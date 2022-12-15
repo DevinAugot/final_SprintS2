320cc68488c6fdc15fde9f531901e8834a9c0040
DevinAugot <105358840+DevinAugot@users.noreply.github.com>
2022-12-15 13:26:16 -0330

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./Nav"
+const Home = () => {
+  return (
+    <div>
+      <Nav></Nav>
+      <div className="home">
+      <h1>This is the home page</h1>
+      </div>
+    </div>
+  );
+};
+export default Home;
